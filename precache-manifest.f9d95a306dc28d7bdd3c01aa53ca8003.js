98eaf9984ec02c13778604b3516ae4b0c4d90ea1
Obradovic Nikola <obradovicnikola009@gmail.com>
2019-08-23 21:22:38 +0200

    Initial commit

---
+++ precache-manifest.f9d95a306dc28d7bdd3c01aa53ca8003.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c5979b5ae71eed2e5d3c97b25aaee7",
+    "url": "//obradovicnikola.github.io/Todo-react/index.html"
+  },
+  {
+    "revision": "d7473dc59aed964af815",
+    "url": "//obradovicnikola.github.io/Todo-react/static/css/main.c335d1eb.chunk.css"
+  },
+  {
+    "revision": "5184162e81abb4bb1e9c",
+    "url": "//obradovicnikola.github.io/Todo-react/static/js/2.fe6a1702.chunk.js"
+  },
+  {
+    "revision": "d7473dc59aed964af815",
+    "url": "//obradovicnikola.github.io/Todo-react/static/js/main.b388d9d1.chunk.js"
+  },
+  {
+    "revision": "7c64253602ede6957614",
+    "url": "//obradovicnikola.github.io/Todo-react/static/js/runtime~main.2c9ba84b.js"
+  }
+]);
