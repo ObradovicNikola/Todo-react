f5cbebf82d6c68c4dc87b28d9b0fa05574227870
Obradovic Nikola <obradovicnikola009@gmail.com>
2019-08-23 21:02:53 +0200

    Initial commit

---
+++ precache-manifest.6cb0675288d20e2546417bf3b089cf3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6147c01801c33b523054ce3b423e6f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f28f4c69847c3e8d35ab",
+    "url": "/static/css/main.db4ddcbb.chunk.css"
+  },
+  {
+    "revision": "5184162e81abb4bb1e9c",
+    "url": "/static/js/2.fe6a1702.chunk.js"
+  },
+  {
+    "revision": "f28f4c69847c3e8d35ab",
+    "url": "/static/js/main.253b9744.chunk.js"
+  },
+  {
+    "revision": "9b717ee3a0372dc14d94",
+    "url": "/static/js/runtime~main.edab03e7.js"
+  }
+]);
